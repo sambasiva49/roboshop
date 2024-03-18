ff5474e95d94fb63e2035b2fc4be0e03572779ca
samba <gudurusamba@gmail.com>
2024-03-18 17:01:57 +0530

    robo shell

--- roboshop-shell/mysql.sh
+++ roboshop-shell/mysql.sh
@@ -1,7 +1,23 @@
+echo -e "\e[36m>>>>>>>>disable nodejs<<<<<<<<<\e[0m"
+
 dnf module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e "\e[36m>>>>>>>>copy the application <<<<<<<<<\e[0m"
+
+cp /home/centos/roboshop/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m>>>>>>>>install mysql<<<<<<<<<\e[0m"
+
 dnf install mysql-community-server -y
+
+echo -e "\e[36m>>>>>>>>enable my mysql <<<<<<<<<\e[0m"
+
 systemctl enable mysqld
-systemctl start mysqld
+echo -e "\e[36m>>>>>>>> Restart Mysql<<<<<<<<<\e[0m"
+
+systemctl restart mysqld
+echo -e "\e[36m>>>>>>>> Mysql password <<<<<<<<<\e[0m"
+
 mysql_secure_installation --set-root-pass RoboShop@1
+echo -e "\e[36m>>>>>>>> Mysql 2nd password <<<<<<<<<\e[0m"
+
 mysql -uroot -pRoboShop@1
