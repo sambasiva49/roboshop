fda38d25f7f4000065c12fb1fe6f7a8ce667a4e6
samba <gudurusamba@gmail.com>
2024-03-18 16:40:28 +0530

    robo shell

--- roboshop-shell/user.sh
+++ roboshop-shell/user.sh
@@ -1,15 +1,42 @@
+echo -e "\e[36m>>>>>>>>disable nodejs<<<<<<<<<\e[0m"
+
+
 dnf module disable nodejs -y
+echo -e "\e[36m>>>>>>>>enable nodejs<<<<<<<<<\e[0m"
+
 dnf module enable nodejs:18 -y
+echo -e "\e[36m>>>>>>>>install nodejs<<<<<<<<<\e[0m"
+
 dnf install nodejs -y
+echo -e "\e[36m>>>>>>>> add Application user <<<<<<<<<\e[0m"
+
 useradd roboshop
+echo -e "\e[36m>>>>>>>>Create Application Directory <<<<<<<<<\e[0m"
+rm -rf /app
+
 mkdir /app
+
+echo -e "\e[36m>>>>>>>> Download App Content<<<<<<<<<\e[0m"
+
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>> unzip App Content <<<<<<<<<\e[0m"
+
 unzip /tmp/user.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>Install nodejs<<<<<<<<<\e[0m"
+
 npm install
-cp user.service /etc/systemd/system/user.service
+
+echo -e "\e[36m>>>>>>>> Copy systemd File<<<<<<<<<\e[0m"
+
+cp /home/centos/roboshop/roboshop-shell/user.service /etc/systemd/system/user.service
+
 systemctl daemon-reload
 systemctl enable user
-systemctl start user
+echo -e "\e[36m>>>>>>>>Restart the user<<<<<<<<<\e[0m"
+
+systemctl restart user
 
