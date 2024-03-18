a22b19a4778eb0d684edeff84d69f931eb21573f
samba <gudurusamba@gmail.com>
2024-03-18 14:58:51 +0530

    robo shell

--- roboshop-shell/catalogue.sh
+++ roboshop-shell/catalogue.sh
@@ -1,18 +1,50 @@
+
+echo -e "\e[36m>>>>>>>>disable nodejs<<<<<<<<<\e[0m"
 dnf module disable nodejs -y
+echo -e "\e[36m>>>>>>>>enable nodejs<<<<<<<<<\e[0m"
+
+
 dnf module enable nodejs:18 -y
+
+echo -e "\e[36m>>>>>>>>install nodejs<<<<<<<<<\e[0m"
+
 dnf install nodejs -y
+echo -e "\e[36m>>>>>>>> add Application user <<<<<<<<<\e[0m"
+
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>Create Application Directory <<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
+
+echo -e "\e[36m>>>>>>>> Download App Content<<<<<<<<<\e[0m"
+
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>> unzip App Content <<<<<<<<<\e[0m"
+
 unzip /tmp/catalogue.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>Install nodejs<<<<<<<<<\e[0m"
+
 npm install
+echo -e "\e[36m>>>>>>>> Copy systemd File<<<<<<<<<\e[0m"
+
 cp catalogue.service /etc/systemd/system/catalogue.service
+
+echo -e "\e[36m>>>>>>>> Start Catalogue Service  <<<<<<<<<\e[0m"
+
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
+echo -e "\e[36m>>>>>>>> Copy Mongo repo<<<<<<<<<\e[0m"
+
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m>>>>>>>> Install Mongod Client  <<<<<<<<<\e[0m"
+
 dnf install mongodb-org-shell -y
 mongo --host mongodb-dev.sambadevops.online </app/schema/catalogue.js
