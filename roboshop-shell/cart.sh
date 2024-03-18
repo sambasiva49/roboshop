7514bbd34b0bd060a3c22a29c8f4a7c20f761786
samba <gudurusamba@gmail.com>
2024-03-18 17:18:14 +0530

    robo shell

--- roboshop-shell/cart.sh
+++ roboshop-shell/cart.sh
@@ -8,7 +8,7 @@ cd /app
 unzip /tmp/cart.zip
 cd /app
 npm install
-cp cart.service /etc/systemd/system/cart.service
+cp /home/centos/roboshop/roboshop-shell/cart.service /etc/systemd/system/cart.service
 systemctl daemon-reload
 systemctl enable cart
 systemctl start cart
