aa9da09c979392424fe4f5f495877fd932a9563a
samba <gudurusamba@gmail.com>
2024-03-18 16:14:49 +0530

    robo shell

--- roboshop-shell/redis.sh
+++ roboshop-shell/redis.sh
@@ -1,7 +1,17 @@
+echo -e "\e[36m>>>>>>>>Install Redis<<<<<<<<<\e[0m"
+
 dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+echo -e "\e[36m>>>>>>>>Enable Redis <<<<<<<<<\e[0m"
+
 dnf module enable redis:remi-6.2 -y
+
+echo -e "\e[36m>>>>>>>>Install Redis<<<<<<<<<\e[0m"
+
 dnf install redis -y
 # i need to edit two files here
 ##You can edit file by using vim /etc/redis.conf & vim /etc/redis/redis.conf
 systemctl enable redis
-systemctl start redis
+echo -e "\e[36m>>>>>>>>Restart the Redis<<<<<<<<<\e[0m"
+
+systemctl restart redis
