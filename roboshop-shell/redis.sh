10558e3289cd17153d4144f953de874167d28c9f
samba <gudurusamba@gmail.com>
2024-03-18 16:21:39 +0530

    robo shell

--- roboshop-shell/redis.sh
+++ roboshop-shell/redis.sh
@@ -9,6 +9,11 @@ dnf module enable redis:remi-6.2 -y
 echo -e "\e[36m>>>>>>>>Install Redis<<<<<<<<<\e[0m"
 
 dnf install redis -y
+
+echo -e "\e[36m>>>>>>>>Update Redis Address<<<<<<<<<\e[0m"
+
+
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
 # i need to edit two files here
 ##You can edit file by using vim /etc/redis.conf & vim /etc/redis/redis.conf
 systemctl enable redis
