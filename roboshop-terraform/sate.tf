b997ebfe02ccbd876099dd18627b81399b084eb8
samba <gudurusamba@gmail.com>
2024-04-20 10:42:13 +0530

    add common.sh file and source all the files to this common.sh

--- roboshop-terraform/sate.tf
+++ roboshop-terraform/sate.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = sambadevops
-    key            = "roboshop/terrafrom.tfstate"
-    region         = "us-east-1"
+
+   # bucket         = sambadevops
+    #key            = "roboshop/terrafrom.tfstate"
+    #region         = "us-east-1"
 
   }
 }
