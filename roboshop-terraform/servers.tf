aa5f5215f30749d9d3ab465faee69de71b8e956e
samba <gudurusamba@gmail.com>
2024-04-22 11:20:57 +0530

    add common.sh file and source all the files to this common.sh

--- roboshop-terraform/servers.tf
+++ roboshop-terraform/servers.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "instance" {
   for_each = var.components
   ami           = data.aws_ami.centos.image_id
   instance_type = each.value["instance_type"]
-  vpc_security_group_ids = [data.aws_security_group.allow_all.id]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
     Name = each.value["name"]
